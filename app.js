0d361cacd6465471862aa967b930e4ed197f98d8
iam-david <vanmelkebekedavid@gmail.com>
2022-10-30 23:13:31 +0100

    Fetching numbers out of page

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const btnSearch = document.getElementById('btn-search');
 
+function searchPage(pageContent) {
+  const regExp =
+    /(BE) [0-9]{4}.[0-9]{3}.[0-9]{3}|(BE)[0-9]{4}.[0-9]{3}.[0-9]{3}|(BE) [0-9]{4}[0-9]{3}[0-9]{3}|(BE)[0-9]{4}[0-9]{3}[0-9]{3}|(BE) [0-9]{4} [0-9]{3} [0-9]{3}|(BE)[0-9]{4} [0-9]{3} [0-9]{3}|[0-9]{4}.[0-9]{3}.[0-9]{3}/g;
+
+  return pageContent.match(regExp);
+}
+
 btnSearch.addEventListener('click', () => {
   const queryOptions = { active: true, currentWindow: true };
 
@@ -9,8 +16,9 @@ btnSearch.addEventListener('click', () => {
       { task: 'kbosearch' },
       function (response) {
         if (!chrome.runtime.lastError) {
-          const content = response;
-          console.log(content);
+          const content = response.replaceAll(/\\n/g, ' ');
+          const match = searchPage(content);
+          console.log(match);
         } else {
           console.log('something went wrong');
         }
