2aff85e3678aee4f409ab229ae8ecf0eff40ff0a
iam-david <vanmelkebekedavid@gmail.com>
2022-10-30 22:16:44 +0100

    fetching page information

--- app.js
+++ app.js
@@ -4,12 +4,16 @@ btnSearch.addEventListener('click', () => {
   const queryOptions = { active: true, currentWindow: true };
 
   chrome.tabs.query(queryOptions, (tabs) => {
-    console.log(tabs[0].id);
     chrome.tabs.sendMessage(
       tabs[0].id,
       { task: 'kbosearch' },
       function (response) {
-        console.log(response.status);
+        if (!chrome.runtime.lastError) {
+          const content = response;
+          console.log(content);
+        } else {
+          console.log('something went wrong');
+        }
       }
     );
   });
--- content.js
+++ content.js
@@ -1,9 +1,5 @@
-const pageContent = document.body;
-
 chrome.runtime.onMessage.addListener((req, sender, sendResponse) => {
   if (req.task === 'kbosearch') {
-    console.log(req, sender);
-    console.log(pageContent);
+    sendResponse(JSON.stringify(document.body.innerText));
   }
-  sendResponse('Done');
 });
