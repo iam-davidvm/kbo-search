a32278c81e0a311462018ceaf57cfd3721b5ca00
iam-david <vanmelkebekedavid@gmail.com>
2022-10-31 13:39:14 +0100

    if sitename has more then 20 chars, use url

--- content.js
+++ content.js
@@ -6,7 +6,7 @@ chrome.runtime.onMessage.addListener((req, sender, sendResponse) => {
     let URL = window.location.href;
     const ArrayURL = URL.split('//')[1].split('.');
     URL = ArrayURL.length >= 3 ? ArrayURL[1] : ArrayURL[0];
-    siteName = siteName ? siteName : URL;
+    siteName = siteName && siteName.length < 20 ? siteName : URL;
     response = {
       info: siteName,
       content: JSON.stringify(document.body.innerText),
